4e268bf435c3a3954a0ff36d94c2120c0c1d05e1
snv <snv.dev.mob@gmail.com>
2025-12-11 06:16:51 +0300

    11/12/25

--- src/math-grades-ML/frontend/flask_app.py
+++ src/math-grades-ML/frontend/flask_app.py
@@ -1,24 +1,66 @@
 from flask import Flask, request, render_template
 import time
+import csv
 
-start = time.time()
+fieldnames = [
+  "OutMarker",
+  "famsize",
+  "Pstatus",
+  "Medu",
+  "Fedu",
+  "Mjob",
+  "Fjob",
+  "guardian",
+  "studytime",
+  "schoolsup",
+  "famsup",
+  "paid",
+  "activities",
+  "nursery",
+  "higher",
+  "internet",
+  "freetime",
+  "health"
+]
+
 
 app = Flask(__name__, static_url_path='/', 
                       static_folder='static',
                       template_folder='templates')
 
+app_version = '1.0'
+CSV_FILE_NAME = app.root_path + '/data/form_data.csv';
+                      
 @app.route('/')
 def index():
-    return render_template('index.html', v='1.a')
+    return render_template('index.html', v=app_version)
+
+@app.route('/form')
+def form():
+    return render_template('form.html', v=app_version)
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html', v=app_version)
 
 @app.route('/send', methods=['POST'])
 def send():
     data = request.form
-    for k, v in data.items():
-        print(k, v)
-    return "OK"
+    new_row_data = [data[k] for k in data]
     
-@app.route('/test')
-def test():
-    return f"Hello from Test! {(time.time()-start)/60:.2f}"
+    # Если файл данных новый, то нужно записать заголовок
+    try:
+        with open(CSV_FILE_NAME, 'x', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
+            writer.writeheader()
+    except FileExistsError:
+        pass
+    
+    # Записываем данные
+    with open(CSV_FILE_NAME, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
+        writer.writerow(data)
+    
+    return render_template('thanks.html', v=app_version)
+
     
--- src/math-grades-ML/frontend/run.py
+++ src/math-grades-ML/frontend/run.py
@@ -13,7 +13,7 @@ if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
-from flask_app import app
+from flask_app import app as application
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
