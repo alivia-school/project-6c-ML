85a4a45d762be5f0a51a29d8a6621f6f3a5466a7
Alivia <soychik.al.ni@school1504.ru>
2026-01-14 01:50:37 +0300

    Signed-off-by: Alivia <soychik.al.ni@school1504.ru>

--- src/math-grades-ML/webapp/main.py
+++ src/math-grades-ML/webapp/main.py
@@ -4,11 +4,7 @@ import csv
 import tensorflow as tf
 from dataset import check_row, processing_row, dataset_values
 
-
-# Версия приложения
 app_version = '1.0'
-
-# Создаем веб приложение
 app = Flask(__name__, static_url_path='/', 
                       static_folder='static',
                       template_folder='templates')
@@ -16,89 +12,64 @@ app = Flask(__name__, static_url_path='/',
 # Загрузка обученной модели
 model = tf.keras.models.load_model(app.root_path + '/model/model.h5')
 
-        
-
-# Функция для маршрута '/'
-# Основная страница, отображается информация о проекте и кнопки сбора данных и прогноза
+CSV_FILE_NAME = app.root_path + '/data/form_data.csv';
+                      
 @app.route('/')
 def index():
     return render_template('index.html', v=app_version)
 
-
-# Функция для маршрута '/form'
-# Страница формы сбора данных для обучения
 @app.route('/form')
 def form():
     return render_template('form.html', v=app_version)
 
-
-# Функция для маршрута '/predict'
-# Страница формы для прогноза
 @app.route('/predict')
 def predict():
     return render_template('predict.html', v=app_version)
 
-
-# Функция для маршрута '/send'
-# Сюда отсылается данные со страницы '/form'
-# Выполняется проверка и сохранение переданных данных в файл
 @app.route('/send', methods=['POST'])
 def send():
     data = request.form.to_dict()
     
-    # Проверка данных формы
     if not check_row(data):
         return "Bad post data"
                 
     fieldnames = list(dataset_values.keys())
-   
-    csv_file_name = app.root_path + '/data/form_data.csv';
+
+    new_row_data = [data[k] for k in data]
    
     # Если файл данных новый, то нужно записать заголовок
     try:
-        with open(csv_file_name, 'x', newline='') as csvfile:
+        with open(CSV_FILE_NAME, 'x', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
             writer.writeheader()
     except FileExistsError:
         pass
     
     # Записываем данные
-    with open(csv_file_name, 'a', newline='') as csvfile:
+    with open(CSV_FILE_NAME, 'a', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
         writer.writerow(data)
     
     return render_template('thanks.html', v=app_version)
     
-
-# Функция для маршрута '/result'
-# Сюда отсылается данные со страницы '/predict'    
-# Выполняется проверка и перевод в параметры для модели переданных данных, 
-# затем затем результат прогноз на их основе отсылается пользователю
+    
 @app.route('/result', methods=['POST'])
 def result():
+    data = request.form.to_dict()
+    row = [list(processing_row(data).values())]    
+
+    predictions = model.predict(row, verbose=False)
+    result_number = predictions[0][0]
     
-    # Проверка и перевод данных формы в параметры для модели 
-    row = processing_row(request.form.to_dict())    
-    if not row:
-        return 'Bad post data'
-    
-    # Прогноз    
-    prediction = model.predict([list(row.values())], verbose=False)[0][0]
-    
-    if prediction < 2.2:
-        result =  'У вас есть вероятность получить неудовлетворительную оценку'
-    elif prediction < 3.2:       
-        result =  'У вас есть вероятность получить "три"'
-    elif prediction < 4.1:       
-        result =  'У вас есть вероятность получить "четыре"'
-    elif prediction < 4.5:       
-        result =  'У вас есть вероятность получить "пять"'
+    if result_number < 2.2:
+        result =  "У вас высокая вероятность получить неудовлетворительную оценку"
+    elif result_number < 3.2:       
+        result =  "У вас есть вероятность получить \"три\""
+    elif result_number < 4.2:       
+        result =  "У вас есть вероятность получить \"четыре\""
+    elif result_number < 4.5:       
+        result =  "У вас есть вероятность получить \"пять\""
     else:       
-        result =  'У вас высокая вероятность получить "пять"'
+        result =  "У вас высока вероятность получить \"пять\""
         
     return render_template('result.html', v=app_version, result=result)
- 
-        
-
-
-    
